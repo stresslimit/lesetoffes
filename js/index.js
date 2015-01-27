10db13628529c1d677a6d5a30c57ff8dc28b1bd7
Colin Vernon <colin@littlebits.cc>
2015-01-27 01:04:13 -0500

    layout, titles, sizing js, etc

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+window.React = require('reactjs/react-bower@v0.12.1:react.js')
 var doc_ready = require('desandro/doc-ready')
 var product_details = require('./product-details')
 var sticky = require('./sticky')
